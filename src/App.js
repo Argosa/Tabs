14892361881f45312f1562cad1e2d8ce5fd6553d
Argosa <ronald.kratochwill@live.com>
2020-03-09 15:22:52 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+// useState returns a state object and a set function
+
+const Button = (props) => {
+
+return (
+  <button 
+  onClick={() => props.textKey (props.incrementValue)} 
+  className='btn'>Tab {props.incrementValue}</button> // event function activates setCounter with counter + 1 that adds 1 to the counter
+  );
+}
+
+const Display = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='display'>
+      {props.message}
+    </div>
+  );
+}
+
+function App() {
+  const [ textNumber, setTextNumber ] = useState(0);
+  const changeLogger = (incValue) => {
+    setTextNumber(incValue)
+  }
+  const textOptions = [
+    'Text for Tab A',
+    'Text for Tab B',
+    'Text for Tab C',
+    'Text for Tab D'
+  ]
+
+  return (
+    <div className="App container bg-light">
+      <Button textKey={() => changeLogger(0)} incrementValue="A"/>
+      <Button textKey={() => changeLogger(1)} incrementValue="B"/>
+      <Button textKey={() => changeLogger(2)} incrementValue="C"/>
+      <Button textKey={() => changeLogger(3)} incrementValue="D"/>
+      <Display message={textOptions[textNumber]}/>
     </div>
   );
 }
